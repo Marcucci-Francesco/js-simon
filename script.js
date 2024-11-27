293935d246316c09a01687426bc9ee028fe21ea9
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-11-27 20:07:23 +0100

    feat: file html

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 10 secondi.
 Dopo 10 secondi i numeri scompaiono e appaiono invece 5 input in cui l’utente deve inserire i numeri che ha visto precedentemente, nell’ordine che preferisce.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
+
+// Inizializzare tutti gli id e le classi che servono
+const countdown = documet.queryselector('countdown');
+const randomNumbers = document.getElementById('number')
+const input = document.querySelectorAll('input');
+const btn = document.querySelector('btn');
+const message = document.getElementById('message');
+
