d4adab0bd8000d580afae03d5d642f8fd6ae5efa
Marcucci-Francesco <fmarcucci47@gmail.com>
2024-12-02 16:02:09 +0100

    feat: add random numbers

--- script.js
+++ script.js
@@ -6,9 +6,39 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 */
 
 // Inizializzare tutti gli id e le classi che servono
-const countdown = documet.queryselector('countdown');
-const randomNumbers = document.getElementById('number')
+const countdownNumber = document.querySelector('.countdown');
+const randomNumbers = document.getElementById('randomNumbers')
 const input = document.querySelectorAll('input');
-const btn = document.querySelector('btn');
+const btn = document.querySelector('.btn');
 const message = document.getElementById('message');
+
+const min = 1;
+const max = 60;
+const totNumbers = 5;
+const timer = 10;
+const totNumbersArray = [];
+let liItems = '';
+
+console.log(totNumbersArray);
+
+// Faccio un ciclo for per creare i numeri random e pusharli in un array vuoto
+for (let i = 0; i < totNumbers; i++) {
+  totNumbersArray.push(Math.floor(Math.random() * (max - min +1) + min)); 
+}
+
+// Faccio un ciclo for per stampare in pagina i numeri casuali dell'array 
+for(let i = 0; i < totNumbersArray.length; i++){
+  const numbers = totNumbersArray[i] 
+  liItems += `<li class="fs-3">${numbers}</li>`;
+}
+randomNumbers.innerHTML = liItems;
+
+
+
+
+
+
+
+
+
 
